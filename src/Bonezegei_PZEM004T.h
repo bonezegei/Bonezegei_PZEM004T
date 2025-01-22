8bb47a1e7e0d80aabf7d68ec1a8515b28058e604
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2025-01-22 19:37:45 +0800

    Update Bonezegei_PZEM004T.h

--- src/Bonezegei_PZEM004T.h
+++ src/Bonezegei_PZEM004T.h
@@ -1,1 +1,27 @@
+/*
+  PZEM004T Library
+  Author: Bonezegei (Jofel Batutay)
+  Date : January 2025
+*/
 
+#include <Arduino.h>
+
+#ifndef _BONEZEGEI_PZEM004T_H_
+#define _BONEZEGEI_PZEM004T_H_
+
+class Bonezegei_PZEM004T {
+public:
+  Bonezegei_PZEM004T();
+  char begin();
+  uint16_t getData(uint16_t);
+  float getVoltage();
+  float getCurrent(bool fahrenheit);
+  int getHumidity();
+
+  uint8_t data[6];
+
+  float _voltage;
+  float _current;
+};
+
+#endif
